4472097d3a5c551182b762c98f677cdd3f4d05d9
banjjak2 <interr0bang@naver.com>
2022-05-26 18:00:43 +0900

    feat: Tmap 서버에 좌표 전달시 해당 위치까지의 걸리는 시간을 반환받도록 구현

--- android/app/src/main/java/com/example/airbnb/viewmodels/HomeViewModel.kt
+++ android/app/src/main/java/com/example/airbnb/viewmodels/HomeViewModel.kt
@@ -4,19 +4,27 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.airbnb.model.City
+import com.example.airbnb.network.TmapRequest
 import com.example.airbnb.repository.HomeRepository
+import com.example.airbnb.repository.TmapRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val homeRepository: HomeRepository
+    private val homeRepository: HomeRepository,
+    private val tmapRepository: TmapRepository
 ) : ViewModel() {
 
-    private val _homeContents:MutableStateFlow<List<City>> = MutableStateFlow(mutableListOf())
+    private val _homeContents: MutableStateFlow<List<City>> = MutableStateFlow(mutableListOf())
     val homeContent: StateFlow<List<City>> = _homeContents.asStateFlow()
+
+    private val _cityTime: MutableStateFlow<Int> = MutableStateFlow(0)
+    val cityTime = _cityTime.asStateFlow()
 
     fun loadHomeContents() {
         viewModelScope.launch {
@@ -25,6 +33,24 @@ class HomeViewModel @Inject constructor(
                 _homeContents.value = cities
                 Log.d("viewModel", _homeContents.value[0].cityName)
             }
+        }
+    }
+
+    fun getTimeToCity(
+        myCoordinate: City.Coordinate,
+        cityCoordinate: City.Coordinate
+    ) {
+        viewModelScope.launch {
+            val tmapData = tmapRepository.getTime(
+                TmapRequest(
+                    myCoordinate.latitude,
+                    myCoordinate.longitude,
+                    cityCoordinate.latitude,
+                    cityCoordinate.longitude
+                )
+            )
+
+            _cityTime.value = tmapData.totalMinute
         }
     }
 }
