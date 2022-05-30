3cdffef88c74481b558692dc29b77307b532a62c
banjjak2 <interr0bang@naver.com>
2022-05-30 21:51:26 +0900

    feat: 프래그먼트간 이동할 때 객체를 전달하기 위해 Parcelable 상속 및 구현

--- android/app/src/main/java/com/example/airbnb/data/CityInfo.kt
+++ android/app/src/main/java/com/example/airbnb/data/CityInfo.kt
@@ -1,5 +1,7 @@
 package com.example.airbnb.data
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.example.airbnb.model.City
 
 data class Image(
@@ -9,4 +11,33 @@ data class Image(
 data class CityInfo(
     val city: City,
     val travelTime: Int = 30
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readParcelable(City::class.java.classLoader) ?: City(
+            "",
+            "",
+            City.Coordinate(0.0, 0.0)
+        ),
+        parcel.readInt()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeParcelable(city, flags)
+        parcel.writeInt(travelTime)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<CityInfo> {
+        override fun createFromParcel(parcel: Parcel): CityInfo {
+            return CityInfo(parcel)
+        }
+
+        override fun newArray(size: Int): Array<CityInfo?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- android/app/src/main/java/com/example/airbnb/model/City.kt
+++ android/app/src/main/java/com/example/airbnb/model/City.kt
@@ -1,12 +1,67 @@
 package com.example.airbnb.model
 
-data class City (
+import android.os.Parcel
+import android.os.Parcelable
+
+data class City(
     val cityName: String,
     val image: String,
     val currentCoordinate: Coordinate
-) {
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readParcelable(Coordinate::class.java.classLoader) ?: Coordinate(0.0, 0.0)
+    ) {
+    }
+
     data class Coordinate(
         val latitude: Double,
         val longitude: Double
-    )
+    ) : Parcelable {
+        constructor(parcel: Parcel) : this(
+            parcel.readDouble(),
+            parcel.readDouble()
+        ) {
+        }
+
+        override fun writeToParcel(parcel: Parcel, flags: Int) {
+            parcel.writeDouble(latitude)
+            parcel.writeDouble(longitude)
+        }
+
+        override fun describeContents(): Int {
+            return 0
+        }
+
+        companion object CREATOR : Parcelable.Creator<Coordinate> {
+            override fun createFromParcel(parcel: Parcel): Coordinate {
+                return Coordinate(parcel)
+            }
+
+            override fun newArray(size: Int): Array<Coordinate?> {
+                return arrayOfNulls(size)
+            }
+        }
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(cityName)
+        parcel.writeString(image)
+        parcel.writeParcelable(currentCoordinate, flags)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<City> {
+        override fun createFromParcel(parcel: Parcel): City {
+            return City(parcel)
+        }
+
+        override fun newArray(size: Int): Array<City?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
