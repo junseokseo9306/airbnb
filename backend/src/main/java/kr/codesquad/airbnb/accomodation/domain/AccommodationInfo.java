27cbc3192ed2bee29023bd12c8a6d2baa844cb1e
Jaehong Choe <qnalsfj@gmail.com>
2022-06-02 15:14:05 +0900

    feat: checkInTime, checkOutTime 변수타입 변경
    - 날짜는 필요가 없어서 LocalDateTime에서 String으로 변경

--- backend/src/main/java/kr/codesquad/airbnb/accomodation/domain/AccommodationInfo.java
+++ backend/src/main/java/kr/codesquad/airbnb/accomodation/domain/AccommodationInfo.java
@@ -1,7 +1,6 @@
 package kr.codesquad.airbnb.accomodation.domain;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
@@ -12,18 +11,13 @@ public class AccommodationInfo {
     private String description;
     private BigDecimal price;
     private String address;
-    @Column(name = "check_in_time")
-    private LocalDateTime checkInTime;
-    @Column(name = "check_out_time")
-    private LocalDateTime checkOutTime;
+    private String checkInTime;
+    private String checkOutTime;
     private Integer occupancy;
     @Column(name = "type")
     private AccommodationType accommodationType;
-    @Column(name = "cleaning_fee")
     private BigDecimal cleaningFee;
-    @Column(name = "bed_count")
     private Integer bedCount;
-    @Column(name = "bathroom_count")
     private Integer bathroomCount;
 
 }
