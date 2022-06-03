f5aab7ea387c5a2c680a9e2cac6c0c8eb16988b9
Jaehong Choe <qnalsfj@gmail.com>
2022-06-03 13:30:59 +0900

    feat: 숙소 검색 조건 dto 추가

--- backend/src/main/java/kr/codesquad/airbnb/accomodation/dto/AccommodationSearchRequest.java
+++ backend/src/main/java/kr/codesquad/airbnb/accomodation/dto/AccommodationSearchRequest.java
@@ -1,5 +1,21 @@
 package kr.codesquad.airbnb.accomodation.dto;
 
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Getter
+@AllArgsConstructor
 public class AccommodationSearchRequest {
+
+    private final String locaiton;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private final LocalDateTime checkIn;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private final LocalDateTime checkOut;
+    private final Integer guests;
+    private final Integer minPrice;
+    private final Integer maxPrice;
 
 }
