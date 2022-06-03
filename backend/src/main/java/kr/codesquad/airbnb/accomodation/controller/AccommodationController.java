35e35b4a1c208203e1a00364712fb6725ca67281
Jaehong Choe <qnalsfj@gmail.com>
2022-06-03 13:42:19 +0900

    feat: controller, service 숙소 조회 코드 작성
    - AccommodationSearchRequest으로 넘겨받은 숙소 검색 조건과 일치하는
    숙소만 조회하려고 하였으나, reservation정보가 필요하다고 판단되어 일단
    전체 숙소리스트 조회하는 기능을 먼저 구현하기로 결정하였음.
    추후 Reservation구현이 완료된 이후에 검색 조건에 일치하는 숙소를 조회하는 기능을
    querydsl로 구현할 예정

--- backend/src/main/java/kr/codesquad/airbnb/accomodation/controller/AccommodationController.java
+++ backend/src/main/java/kr/codesquad/airbnb/accomodation/controller/AccommodationController.java
@@ -4,8 +4,6 @@ import kr.codesquad.airbnb.accomodation.dto.AccommodationSearchRequest;
 import kr.codesquad.airbnb.accomodation.dto.AccommodationsResponse;
 import kr.codesquad.airbnb.accomodation.service.AccommodationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,13 +16,9 @@ public class AccommodationController {
     private final AccommodationService accommodationService;
 
     @GetMapping("/accommodations")
-    public ResponseEntity<AccommodationsResponse> getAccommodations(
+    public AccommodationsResponse getAccommodations(
         AccommodationSearchRequest accommodationSearchRequest){
-        AccommodationsResponse accommodationResponse = accommodationService.findAll(accommodationSearchRequest);
-
-        return new ResponseEntity(accommodationResponse, HttpStatus.OK);
-
-
+        return accommodationService.findAll(accommodationSearchRequest);
     }
 
 }
--- backend/src/main/java/kr/codesquad/airbnb/accomodation/service/AccommodationService.java
+++ backend/src/main/java/kr/codesquad/airbnb/accomodation/service/AccommodationService.java
@@ -1,6 +1,10 @@
 package kr.codesquad.airbnb.accomodation.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import kr.codesquad.airbnb.accomodation.domain.Accommodation;
 import kr.codesquad.airbnb.accomodation.domain.AccommodationRepository;
+import kr.codesquad.airbnb.accomodation.dto.AccommodationResponse;
 import kr.codesquad.airbnb.accomodation.dto.AccommodationSearchRequest;
 import kr.codesquad.airbnb.accomodation.dto.AccommodationsResponse;
 import lombok.RequiredArgsConstructor;
@@ -10,11 +14,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AccommodationService {
 
-    private AccommodationRepository accommodationRepository;
+    private final AccommodationRepository accommodationRepository;
 
-    public AccommodationsResponse findAll(
-        AccommodationSearchRequest accommodationSearchRequest) {
+    public AccommodationsResponse findAll(AccommodationSearchRequest accommodationSearchRequest) {
+        List<AccommodationResponse> accommodations = accommodationRepository.findAll()
+            .stream()
+            .map(Accommodation::convertToAccommodationResponse)
+            .collect(Collectors.toList());
 
-        return null;
+        return new AccommodationsResponse(accommodations);
     }
 }
