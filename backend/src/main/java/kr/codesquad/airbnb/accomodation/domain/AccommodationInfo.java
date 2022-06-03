6ec04089729424b506aa7b8f902c3bb4e2ce644a
Jaehong Choe <qnalsfj@gmail.com>
2022-06-03 13:27:15 +0900

    feat: 숙소 타입 Enum멤버에 @Eunumerated 어노테이션 추가

--- backend/src/main/java/kr/codesquad/airbnb/accomodation/domain/AccommodationInfo.java
+++ backend/src/main/java/kr/codesquad/airbnb/accomodation/domain/AccommodationInfo.java
@@ -3,6 +3,8 @@ package kr.codesquad.airbnb.accomodation.domain;
 import java.math.BigDecimal;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import lombok.Getter;
 
 @Embeddable
@@ -17,6 +19,7 @@ public class AccommodationInfo {
     private String checkOutTime;
     private Integer occupancy;
     @Column(name = "type")
+    @Enumerated(EnumType.STRING)
     private AccommodationType accommodationType;
     private BigDecimal cleaningFee;
     private Integer bedCount;
