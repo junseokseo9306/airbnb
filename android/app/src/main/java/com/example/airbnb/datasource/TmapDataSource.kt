25921f464335725590bfed57ca797fa030eebe53
banjjak2 <interr0bang@naver.com>
2022-06-05 00:06:38 +0900

    refact: 오타 수정 및 Dispatchers 변경
    Json 형식의 데이터를 파싱하므로 Dispatcher를 Default로 변경

--- android/app/src/main/java/com/example/airbnb/datasource/TmapDataSource.kt
+++ android/app/src/main/java/com/example/airbnb/datasource/TmapDataSource.kt
@@ -2,9 +2,10 @@ package com.example.airbnb.datasource
 
 import com.example.airbnb.dto.TmapDto
 import com.example.airbnb.network.TmapRequest
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 
 interface TmapDataSource {
 
-    fun getTime(tmapRequest: TmapRequest): Flow<TmapDto>
+    fun getTime(tmapRequest: TmapRequest, dispatcher: CoroutineDispatcher): Flow<TmapDto>
 }
--- android/app/src/main/java/com/example/airbnb/repository/TmapRepositoryImpl.kt
+++ android/app/src/main/java/com/example/airbnb/repository/TmapRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.example.airbnb.datasource.TmapDataSource
 import com.example.airbnb.dto.toTmap
 import com.example.airbnb.model.Tmap
 import com.example.airbnb.network.TmapRequest
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -12,7 +13,7 @@ class TmapRepositoryImpl @Inject constructor(
     private val tmapDataSource: TmapDataSource
 ) : TmapRepository {
     override fun getTime(tmapRequest: TmapRequest): Flow<Tmap> =
-        tmapDataSource.getTime(tmapRequest).map { tampDto ->
-            tampDto.toTmap()
+        tmapDataSource.getTime(tmapRequest, Dispatchers.Default).map { tmapDto ->
+            tmapDto.toTmap()
         }
 }
