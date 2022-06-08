8d4444597bfba91f90f88c9939c50fb24d7b9d2d
banjjak2 <interr0bang@naver.com>
2022-06-08 18:43:17 +0900

    feat: API 명세 변경에 따른 수정 진행

--- android/app/src/main/java/com/example/airbnb/data/SearchFilter.kt
+++ android/app/src/main/java/com/example/airbnb/data/SearchFilter.kt
@@ -2,10 +2,28 @@ package com.example.airbnb.data
 
 import java.io.Serializable
 import androidx.core.util.Pair
+import com.example.airbnb.common.timestampToDateString
+import com.example.airbnb.dto.SearchFilterDto
 
 data class SearchFilter(
     val location: String,
     var checkInOut: Pair<Long, Long>?,
-    var guests: Int?,
-    var priceRange: Pair<Int, Int>?
-): Serializable
+    val guests: Int?,
+    val priceRange: Pair<Int, Int>?
+) : Serializable
+
+fun SearchFilter.toDto(): SearchFilterDto {
+    val checkIn = if (checkInOut == null) null else timestampToDateString(checkInOut!!.first)
+    val checkOut = if (checkInOut == null) null else timestampToDateString(checkInOut!!.second)
+    val minPrice = priceRange?.first
+    val maxPrice = priceRange?.second
+
+    return SearchFilterDto(
+        location,
+        checkIn,
+        checkOut,
+        guests,
+        minPrice,
+        maxPrice
+    )
+}
