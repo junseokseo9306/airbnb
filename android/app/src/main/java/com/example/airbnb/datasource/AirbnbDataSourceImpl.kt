cd48817e86c4e18a64223794f502a0d36d79ae75
banjjak2 <interr0bang@naver.com>
2022-06-10 13:57:40 +0900

    fix: 서버 URL 수정

--- android/app/src/main/java/com/example/airbnb/datasource/AirbnbDataSourceImpl.kt
+++ android/app/src/main/java/com/example/airbnb/datasource/AirbnbDataSourceImpl.kt
@@ -15,81 +15,23 @@ import javax.inject.Singleton
 class AirbnbDataSourceImpl @Inject constructor(private val api: AirbnbApi) : AirbnbDataSource {
 
     override suspend fun getCityDto(): CityDto {
-//        return api.getCityDto()
-
-        return CityDto(
-            listOf(
-                CityDto.Local(
-                    "서울",
-                    CityDto.Coordinate(
-                        127.333,
-                        37.111
-                    ),
-                    null
-                )
-            )
-        )
+        return api.getCityDto()
     }
 
     override suspend fun getAccommodations(
         searchFilterDto: SearchFilterDto,
         dispatcher: CoroutineDispatcher
     ): AccommodationDto {
-//        return withContext(dispatcher) {
-//            api.getAccommodations(
-//                searchFilterDto.location,
-//                searchFilterDto.checkIn,
-//                searchFilterDto.checkOut,
-//                searchFilterDto.guests,
-//                searchFilterDto.minPrice,
-//                searchFilterDto.maxPrice
-//            )
-//        }
-
-        return AccommodationDto(
-            3,
-            listOf(
-                Accommodation(
-                    1,
-                    "헬스장이 딸려있는 피오의 집",
-                    100000,
-                    1000000,
-                    4.8,
-                    473,
-                    true,
-                    true,
-                    "image/A_1.png",
-                    37.476941,
-                    127.040453
-                ),
-                Accommodation(
-                    2,
-                    "헬스장이 딸려있는 반스의 집",
-                    110000,
-                    1100000,
-                    4.9,
-                    411,
-                    false,
-                    true,
-                    "image/B_1.png",
-                    37.475238,
-                    127.037706
-                ),
-                Accommodation(
-                    3,
-                    "비싼 집",
-                    200000,
-                    2000000,
-                    4.6,
-                    811,
-                    true,
-                    false,
-                    "image/C_1.png",
-                    37.473399,
-                    127.039423
-                ),
+        return withContext(dispatcher) {
+            api.getAccommodations(
+                searchFilterDto.location,
+                searchFilterDto.checkIn,
+                searchFilterDto.checkOut,
+                searchFilterDto.guests,
+                searchFilterDto.minPrice,
+                searchFilterDto.maxPrice
             )
-        )
+        }
     }
 
     // TODO : Price Range API Request
--- android/app/src/main/java/com/example/airbnb/di/NetworkModule.kt
+++ android/app/src/main/java/com/example/airbnb/di/NetworkModule.kt
@@ -19,7 +19,7 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
-    private const val BASE_URL = "https://d08319fb-0433-4e9e-a6fb-4a8b2bcee2b7.mock.pstmn.io"
+    private const val BASE_URL = "https://141d49a8-e134-4530-b6e9-26254875e6dd.mock.pstmn.io/"
     private const val TMAP_URL = "https://apis.openapi.sk.com/tmap/"
     const val HERO_IMAGE_URL =
         "https://news.airbnb.com/wp-content/uploads/sites/4/2019/06/PJM020719Q202_Luxe_WanakaNZ_LivingRoom_0264-LightOn_R1.jpg?w=1536"
