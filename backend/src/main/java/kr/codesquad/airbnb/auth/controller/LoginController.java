9ae8fd7e0f3d012bcebce96cd95614cc28e21dff
Jaehong Choe <qnalsfj@gmail.com>
2022-06-10 14:49:56 +0900

    feat: 로그인 상태 확인 메서드 추가

--- backend/src/main/java/kr/codesquad/airbnb/auth/controller/LoginController.java
+++ backend/src/main/java/kr/codesquad/airbnb/auth/controller/LoginController.java
@@ -4,6 +4,8 @@ import kr.codesquad.airbnb.auth.AccessToken;
 import kr.codesquad.airbnb.auth.GitHubUserDto;
 import kr.codesquad.airbnb.auth.service.LoginService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +28,19 @@ public class LoginController {
 
         String userId = userInfo.getUserId();
         httpSession.setAttribute("userId", userId);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<String> getUserId(HttpSession httpSession) {
+        String userId = (String) httpSession.getAttribute("userId");
+
+        if (userId == null) {
+//            ResponseMessage message = new ResponseMessage(HttpStatus.UNAUTHORIZED, "로그인이 되어있지 않습니다.");
+            return new ResponseEntity<>("로그인이 되어있지 않습니다.", HttpStatus.UNAUTHORIZED);
+        }
+
+//        ResponseMessage message = new ResponseMessage(HttpStatus.OK, "로그인이 정상적으로 처리되었습니다.", email);
+        return new ResponseEntity<>("로그인이 정상적으로 처리되었습니다.", HttpStatus.OK);
     }
 
 }
