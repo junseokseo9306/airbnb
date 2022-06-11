6f9835cc7b8549f1cca319c69adee085eab46c4a
junseokseo <junseoks9306@gmail.com>
2022-06-11 23:17:19 +0900

    feat: google map marker 선택시 viewpager 움직이기 설정

--- android/app/src/main/java/com/example/airbnb/ui/map/GoogleMapFragment.kt
+++ android/app/src/main/java/com/example/airbnb/ui/map/GoogleMapFragment.kt
@@ -7,14 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.airbnb.R
 import com.example.airbnb.adapters.SearchListAdapter
 import com.example.airbnb.data.Accommodation
 import com.example.airbnb.databinding.FragmentGoogleMapBinding
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,6 +23,8 @@ class GoogleMapFragment : Fragment(), OnMapReadyCallback {
     private lateinit var binding: FragmentGoogleMapBinding
     private lateinit var mapView: MapView
     private lateinit var adapter: SearchListAdapter
+    private lateinit var accommodationsList: List<Accommodation>
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,35 +37,62 @@ class GoogleMapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         val incomingData = arguments?.getSerializable("accommodationList") as List<Accommodation>
-
+        accommodationsList = incomingData
         adapter = SearchListAdapter(false).apply {
             submitList(incomingData)
         }
-
         binding.vpAccommodationList.adapter = adapter
-
         mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+        viewPagerMove()
+        markerClick()
     }
 
     override fun onMapReady(p0: GoogleMap) {
-        Log.d("GoogleMap", "Is on start")
-        val seoul = LatLng(37.566, 126.978)
-        p0.moveCamera(CameraUpdateFactory.newLatLng(seoul))
-        p0.moveCamera(CameraUpdateFactory.zoomTo(10f))
-        val marker = MarkerOptions()
-            .position(seoul)
-            .title("서울")
-            .snippet("아름다운 도시")
-        p0.addMarker(marker)
-        Log.d("GoogleMap", "Is on")
+        val firstLocation = LatLng(accommodationsList[0].latitude, accommodationsList[0].longitude)
+        p0.moveCamera(CameraUpdateFactory.newLatLng(firstLocation))
+        p0.moveCamera(CameraUpdateFactory.zoomTo(15f))
+        accommodationsList.forEach { accommodation ->
+            val location = LatLng(accommodation.latitude, accommodation.longitude)
+            val marker = MarkerOptions()
+                .position(location)
+                .title(accommodation.name)
+                .snippet(accommodation.name)
+            p0.addMarker(marker)
+        }
     }
+
+    private fun viewPagerMove() {
+        binding.vpAccommodationList.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                val selectedPage = adapter.currentList[position]
+                mapView.getMapAsync { googleMap ->
+                    val cameraUpdate = CameraUpdateFactory.newLatLng(LatLng(selectedPage.latitude, selectedPage.longitude))
+                    googleMap.animateCamera(cameraUpdate)
+                }
+            }
+        })
+    }
+
+    private fun markerClick() {
+        mapView.getMapAsync { googleMap ->
+            googleMap.setOnMarkerClickListener { marker ->
+                val index = adapter.currentList.firstOrNull() { accommodation ->
+                    accommodation.latitude == marker.position.latitude
+                }
+                val position = adapter.currentList.indexOf(index)
+                binding.vpAccommodationList.currentItem = position
+                true
+            }
+        }
+    }
+
 
     override fun onStart() {
         super.onStart()
         mapView.onStart()
-        mapView.getMapAsync(this)
     }
 
     override fun onResume() {
